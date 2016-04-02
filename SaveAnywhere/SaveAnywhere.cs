1b82dfbe3bf59060f8dd9ae6253600487e243eeb
tyler staples <tyler.a.staples@gmail.com>
2016-04-01 18:31:37 -0700

    added project and manifest files

---
+++ SaveAnywhere/SaveAnywhere.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace SaveAnywhere
+{
+    public class SaveAnywhere : Mod
+    {
+        private const int SaveCompleteFlag = 100;
+
+        public override void Entry(params object[] objects)
+        {
+        }
+
+        private void Save()
+        {
+            IEnumerator<int> saveEnumerator = SaveGame.Save();
+            while (saveEnumerator.MoveNext())
+            {
+                if (saveEnumerator.Current == SaveCompleteFlag)
+                {
+                    Log.Debug("Finished saving");
+                }
+            }
+        }
+    }
+}
