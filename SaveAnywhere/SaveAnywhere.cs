da49d3f36c94649aaf6f0c26a6b0d729dad1de8b
tyler staples <tyler.a.staples@gmail.com>
2016-04-01 19:38:36 -0700

    added button creation and click logic. untested since the button isn't being drawn yet

--- SaveAnywhere/SaveAnywhere.cs
+++ SaveAnywhere/SaveAnywhere.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
@@ -16,6 +17,7 @@ namespace SaveAnywhere
         private bool isGameMenuOpen = false;
         private bool pollForExitPage = false;
         private bool isExitPageOpen = false;
+        private Rectangle saveButtonBounds;
 
         private IClickableMenu previousMenu = null;
         private bool wasMenuClosedInvoked = false;
@@ -26,15 +28,28 @@ namespace SaveAnywhere
             GameEvents.UpdateTick += OnUpdateTick;
         }
 
+        // TODO: add gamepad support
         private void OnMouseChanged(object sender, EventArgsMouseStateChanged e)
         {
-            if (e.NewState.LeftButton == ButtonState.Pressed && Game1.activeClickableMenu != null)
+            if (e.NewState.LeftButton != ButtonState.Pressed)
+                return;
+
+                // Probably don't need to check if the menu is null since we wouldn't doing this check if it was
+            if (!isExitPageOpen && Game1.activeClickableMenu != null)
             {
                 GameMenu gameMenu = (GameMenu)Game1.activeClickableMenu;
                 // The tab won't switch until next update so set a flag to check next update
-                if (gameMenu.currentTab != GameMenu.exitTab && !isExitPageOpen && !pollForExitPage)
+                if (gameMenu.currentTab != GameMenu.exitTab && !pollForExitPage)
                 {
                     pollForExitPage = true;
+                }
+            }
+            else
+            {
+                Debug.Assert(isExitPageOpen, "exit page should be open if we've reached this point");
+                if (saveButtonBounds.Contains(e.NewState.X, e.NewState.Y))
+                {
+                    Save();
                 }
             }
         }
@@ -85,6 +100,13 @@ namespace SaveAnywhere
 
             var pages = Utils.GetNativeField<List<IClickableMenu>, GameMenu>(gameMenu, "pages");
             ExitPage exitPage = (ExitPage)pages[gameMenu.currentTab];
+
+            int x = exitPage.xPositionOnScreen + Game1.tileSize * 3 + Game1.tileSize / 2;
+            int y = exitPage.yPositionOnScreen + Game1.tileSize * 4 - Game1.tileSize / 2;
+            int w = Game1.tileSize * 5;
+            int h = Game1.tileSize * 3 / 2;
+            saveButtonBounds = new Rectangle(x, y, w, h);
+            var saveButton = new ClickableComponent(saveButtonBounds, "Save Game");
         }
 
         // TODO: create/store backups of players saves first
