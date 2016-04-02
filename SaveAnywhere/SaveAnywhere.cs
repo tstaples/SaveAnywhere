4945f17171342b93f462cc84cc725cbfd3786bf5
tyler staples <tyler.a.staples@gmail.com>
2016-04-01 19:22:45 -0700

    added initial logic for checking when the exit page is opened

--- SaveAnywhere/SaveAnywhere.cs
+++ SaveAnywhere/SaveAnywhere.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
+using StardewValley.Menus;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace SaveAnywhere
 {
@@ -10,10 +13,82 @@ namespace SaveAnywhere
     {
         private const int SaveCompleteFlag = 100;
 
+        private bool isGameMenuOpen = false;
+        private bool pollForExitPage = false;
+        private bool isExitPageOpen = false;
+
+        private IClickableMenu previousMenu = null;
+        private bool wasMenuClosedInvoked = false;
+
         public override void Entry(params object[] objects)
         {
+            MenuEvents.MenuChanged += OnMenuChanged;
+            GameEvents.UpdateTick += OnUpdateTick;
         }
 
+        private void OnMouseChanged(object sender, EventArgsMouseStateChanged e)
+        {
+            if (e.NewState.LeftButton == ButtonState.Pressed && Game1.activeClickableMenu != null)
+            {
+                GameMenu gameMenu = (GameMenu)Game1.activeClickableMenu;
+                // The tab won't switch until next update so set a flag to check next update
+                if (gameMenu.currentTab != GameMenu.exitTab && !isExitPageOpen && !pollForExitPage)
+                {
+                    pollForExitPage = true;
+                }
+            }
+        }
+
+        private void OnMenuChanged(object sender, EventArgsClickableMenuChanged e)
+        {
+            // Reset flag
+            wasMenuClosedInvoked = false;
+            previousMenu = e.PriorMenu;
+
+            if (Utils.IsType<GameMenu>(e.NewMenu) && !isGameMenuOpen)
+            {
+                isGameMenuOpen = true;
+                ControlEvents.MouseChanged += OnMouseChanged;
+            }
+        }
+
+        private void OnClickableMenuClosed(IClickableMenu priorMenu)
+        {
+            isGameMenuOpen = false;
+            isExitPageOpen = false;
+            ControlEvents.MouseChanged -= OnMouseChanged;
+        }
+
+        private void OnUpdateTick(object sender, EventArgs e)
+        {
+            if (!wasMenuClosedInvoked && previousMenu != null && Game1.activeClickableMenu == null)
+            {
+                wasMenuClosedInvoked = true;
+                OnClickableMenuClosed(previousMenu);
+            }
+
+            if (pollForExitPage)
+            {
+                GameMenu gameMenu = (GameMenu)Game1.activeClickableMenu;
+                if (gameMenu.currentTab == GameMenu.exitTab && !isExitPageOpen)
+                {
+                    isExitPageOpen = true;
+                    pollForExitPage = false;
+                    OnExitPageOpened(gameMenu);
+                }
+            }
+        }
+
+        private void OnExitPageOpened(GameMenu gameMenu)
+        {
+            Log.Debug("Exit tab clicked");
+
+            var pages = Utils.GetNativeField<List<IClickableMenu>, GameMenu>(gameMenu, "pages");
+            ExitPage exitPage = (ExitPage)pages[gameMenu.currentTab];
+        }
+
+        // TODO: create/store backups of players saves first
+        // and maybe store up to 5 backups or something
         private void Save()
         {
             IEnumerator<int> saveEnumerator = SaveGame.Save();
---
+++ SaveAnywhere/Utils.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+
+namespace SaveAnywhere
+{
+    public class Utils
+    {
+        public static void DebugLog(string message)
+        {
+#if DEBUG && SMAPI_VERSION_39_3_AND_PRIOR
+            // Log.Debug doesn't work in 39.2 since defining DEBUG breaks stuff...
+            Log.Info(message);
+#elif DEBUG
+            Log.Debug(message);
+#endif
+        }
+
+        public static string ArrayToString<T>(T[] array)
+        {
+            string s = "";
+            int i = 0;
+            foreach (T item in array)
+            {
+                s += item.ToString() + ((++i < array.Length) ? ", " : "");
+            }
+            return s;
+        }
+
+        public static T[] ConcatArrays<T>(T[] a, T[] b)
+        {
+            T[] c = new T[a.Length + b.Length];
+            Array.Copy(a, c, a.Length);
+            Array.Copy(b, c, b.Length);
+            return c;
+        }
+
+        public static int[] StringToIntArray(string[] array, int defaultVal=0)
+        {
+            int[] output = new int[array.Length];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i] == null || !array[i].IsInt32())
+                    continue;
+
+                try
+                {
+                    output[i] = Int32.Parse(array[i]);
+                }
+                catch (Exception ex)
+                {
+                    Utils.DebugLog("[CalendarBirthdayGiftHelper] failed to convert " + array[i] + "to int32: " + ex);
+                    output[i] = defaultVal;
+                }
+            }
+            return output;
+        }
+
+        public static T GetNativeField<T, Instance>(Instance instance, string fieldName)
+        {
+            FieldInfo fieldInfo = typeof(Instance).GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            return (T)fieldInfo.GetValue(instance);
+        }
+
+        public static bool IsType<T>(object o)
+        {
+            if (o != null)
+            {
+                return (o.GetType() == typeof(T));
+            }
+            return false;
+        }
+
+        public static int GetTileSheetIndexFromID(int id)
+        {
+            if (id == 0)
+                return 0;
+
+            const int spriteSize = 16; // each sprite on this sheet is 16x16
+            int x = (int)Math.Floor((float)(id / 24.0f));
+            int y = id % spriteSize;
+            return (y * spriteSize) + x;
+        }
+
+        public static int Clamp(int val, int min, int max)
+        {
+            return Math.Max(Math.Min(val, max), min);
+        }
+    }
+}
