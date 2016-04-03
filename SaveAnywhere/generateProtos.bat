c7e1eb80368758ba2e6c3340a24d94a0504033fa
tyler staples <tyler.a.staples@gmail.com>
2016-04-02 19:53:33 -0700

    moved script back to src dir, so fixed the paths to match

--- SaveAnywhere/generateProtos.bat
+++ SaveAnywhere/generateProtos.bat
@@ -3,7 +3,7 @@ setlocal
 
 set protoc="C:\Users\Tyler\Documents\Programming\C#\protoc\protoc.exe"
 
-%protoc% --proto_path=SaveAnywhere/ProtoFiles^
-	--csharp_out=SaveAnywhere/GeneratedProtoFiles^
-	SaveAnywhere/ProtoFiles/Common.proto^
-	SaveAnywhere/ProtoFiles/Game.proto
+%protoc% --proto_path=ProtoFiles^
+	--csharp_out=GeneratedProtoFiles^
+	ProtoFiles/Common.proto^
+	ProtoFiles/Game.proto
